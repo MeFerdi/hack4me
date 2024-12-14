43df470a44f998432d71a310f7fd94d55bdaac53
MeFerdi <ferdinandodhis254@gmail.com>
2024-12-15 00:51:44 +0300

    feat: redirect frontend to connect with backend data

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -178,7 +178,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ab9ead30426c6d03")
+/******/ 		__webpack_require__.h = () => ("9973de42ad363a04")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
