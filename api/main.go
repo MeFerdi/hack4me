07d1aaf3183cb302832983a761bf77ca3f66b02c
MeFerdi <ferdinandodhis254@gmail.com>
2024-12-14 14:28:52 +0300

    feat: move main to api dir for backend deployment

--- api/main.go
+++ api/main.go
@@ -27,16 +27,13 @@ func main() {
 	}))
 
 	// Define the /signin route
-	router.POST("/signup", controllers.SignUp)	
+	router.POST("/signup", controllers.SignUp)
 	router.POST("/login", controllers.Login)
 	router.POST("/satellite", controllers.Satelitte)
 	router.POST("/satelitteLogin", controllers.SatelitteLogin)
 	router.POST("/satelitteDashboard/add", controllers.DonateBlood)
 	//router.GET("/region", controllers.Region)
 	router.GET("/donorPage/:userID", controllers.GetUserDonations)
-
-
-
 
 	// Run the server on port 3000
 	router.Run(":3000")
